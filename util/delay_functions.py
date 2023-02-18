ee1e35791e13a6be9d8aa407b6fb13158ca97ac2
Harlok <holmik90@yandex.ru>
2023-02-18 15:35:03 +0300

    upd

---
+++ util/delay_functions.py
@@ -0,0 +1,8 @@
+import asyncio
+
+
+async def delay(delay_seconds: int) -> int:
+    print(f'засыпаю на {delay_seconds} с')
+    await asyncio.sleep(delay_seconds)
+    print(f'сон в течение {delay_seconds} с закончился')
+    return delay_seconds
