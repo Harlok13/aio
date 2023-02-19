8f6f4f16040f5ce5295a1a6925924548def980df
Harlok <holmik90@yandex.ru>
2023-02-19 23:53:05 +0300

    upd

--- bot.py
+++ bot.py
@@ -3,11 +3,20 @@ import logging
 from aiogram import Bot, Dispatcher, types, executor
 from config import TOKEN_API
 
+HELP_COMMAND = """
+/start - начать работу с ботом
+/help - список команд
+/description - описание бота
+"""
 
+BOT_DESCRIPTION = """
+Этот милый бот плод моего творения,
+наслаждайтесь его работой:)
+"""
 # логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # объект бота
-bot = Bot(token=TOKEN_API )
+bot = Bot(token=TOKEN_API)
 # Диспетчер
 dp = Dispatcher(bot)
 
@@ -22,7 +31,21 @@ dp = Dispatcher(bot)
 #     await dp.start_polling(bot)
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(text=message.text.upper())
+    import random, string
+    bot_message = random.choice(string.ascii_lowercase)
+
+    await message.answer(text=bot_message)
+
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.reply(text=HELP_COMMAND)
+
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer(text='Добро пожаловать!')
+    await message.delete()
 
 
 if __name__ == '__main__':
