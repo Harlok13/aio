bde78976f8b198bfe856d5f5da16eead3841fc95
Harlok <holmik90@yandex.ru>
2023-02-22 00:10:40 +0300

    study(bot/bot): fix callback_random_photo

--- bot/bot.py
+++ bot/bot.py
@@ -259,9 +259,9 @@ async def random_phrases_commands(message: types.Message):
 async def callback_random_photo(callback: types.CallbackQuery):
     """Обрабатывает колбеки при нажатии на инлайн клавиатуре send_random_photo"""
     if callback.data == 'like':
-        await callback.message.answer('Вам понравилось!')
+        await callback.answer('Вам понравилось!')
     elif callback.data == 'dislike':
-        await callback.message.answer('Вам не понравилось!')
+        await callback.answer('Вам не понравилось!')
     else:
         await send_random(message=callback.message)
         await callback.answer()
