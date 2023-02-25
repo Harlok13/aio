be5bac75d1c62539eff3b6db7315d8183aae661e
Harlok <holmik90@yandex.ru>
2023-02-25 09:58:10 +0300

    feat(julia/handlers): add filter dirty_words

--- julia/handlers/message_handler.py
+++ julia/handlers/message_handler.py
@@ -1,10 +1,20 @@
+import json
+import random
+import string
+
 from julia.dispatcher import *
 from aiogram import types
 
 
-# @dp.message_handler()
 async def start_cmd(message: types.Message):
-    await message.answer(message.text)
+    print(message)
+    if {word.lower().translate(str.maketrans('', '', string.punctuation)) for word in message.text.split()} \
+        .intersection(set(json.load(open('cenz.json')))) != set():
+        mes = ['Ну к чему этот мат, господа', 'Выбирайте выражения, товарищи', 'Попрошу без мата, милорды', 'Почему вы ругаетесь, мисье']
+        await message.reply(random.choice(mes))
+        return await message.delete()
+
+    await bot.send_message(message.chat.id, message.text)
 
 
 def register_message_handler(dp: Dispatcher):
