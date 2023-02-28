f5e6c753c41286c3d97253dcdabaf14df883f267
Harlok <holmik90@yandex.ru>
2023-02-28 11:47:10 +0300

    feat(julia/handlers): add get_gif_cmd

--- julia/handlers/cmd_handler.py
+++ julia/handlers/cmd_handler.py
@@ -1,9 +1,14 @@
+import random
+
 from aiogram import types
-from aiogram.types import InputFile
+from aiogram.filters import Command
+import aiohttp
 
 from julia.dispatcher import *
 from julia.keyboards.reply_keyboard import *
 from julia.keyboards.inline_keyboard import *
+
+RANDOM_GIF = f'https://api.giphy.com/v1/gifs/random?api_key={os.getenv("GIPHY_API")}&tag=&rating=g'
 
 
 async def start_cmd(message: types.Message):
@@ -22,6 +27,27 @@ async def get_cat_cmd(message: types.Message):
     #                      photo=res['file'],
     #                      reply_markup=GET_CAT)
     await message.delete()
+
+
+async def get_gif_cmd(message: types.Message):
+    # logger = logging.getLogger(__name__)
+    async with aiohttp.ClientSession() as session:
+        param = {'api_key': os.getenv('GIPHY_API'),
+                 'q': 'cat',
+                 'limit': 25,
+                 'offset': random.randrange(1000),
+                 'rating': 'g',
+                 'language': 'en'}
+        async with session.get(
+                f'https://api.giphy.com/v1/gifs/search?api_key={os.getenv("GIPHY_API")}&q=cat&limit=25&offset={random.randrange(1000)}&rating=g&lang=en') as resp:
+            res = await resp.json()
+    await message.answer_animation(animation=res['data'][0]['images']['original']['mp4'])
+    # await bot.send_animation(message.chat.id,
+    #                          animation=res['data'][0]['images']['original']['mp4'])
+    await message.delete()
+
+
+# ['data']['images']['original_mp4']['mp4']
 
 
 async def info_cmd(message: types.Message):
