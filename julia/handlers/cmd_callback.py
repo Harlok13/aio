e8a2fff25ad566020877c579a120bb4559f39c2c
Harlok <holmik90@yandex.ru>
2023-02-28 11:49:20 +0300

    ref(julia/handlers): ref to aiogram 3

--- julia/handlers/cmd_callback.py
+++ julia/handlers/cmd_callback.py
@@ -1,6 +1,9 @@
+import aiohttp
+
 from aiogram import types
-from aiogram.types import InputFile
-from aiogram.utils.exceptions import BadRequest
+from aiogram.filters import Command
+
+from julia.keyboards.inline_keyboard import *
 
 from julia.dispatcher import *
 
@@ -11,6 +14,8 @@ ERROR_FIND_BOOK = 'Ой, такой книги тут нет:('
 
 MY_CHAT_ID = -1001835718066
 MY_ID = 5485747686
+CHAT_ED = -1001889081173
+
 
 
 # реализовать бд с помощью redis. сохранять словарь с id чата, сообщения, чтобы потом по ним искать книгу
@@ -18,14 +23,30 @@ async def callback_book_info(callback: types.CallbackQuery):
     logger = logging.getLogger(__name__)
     logger.info(callback.data)
     call = await redis.get(callback.data)
-    try:
-        await bot.edit_message_text(text=f"Вот ссылка на вашу книгу:\n{str(call).lstrip('b')}".replace("'", ""),
-                                    chat_id=callback.message.chat.id,
-                                    message_id=callback.message.message_id)
-    except Exception as exc:
-        logger.info(exc)
-        print(f'error {exc}')
+    logger.info(call)
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://aws.random.cat/meow') as resp:
+            res: dict = await resp.json()
+            print(res['file'])
+    if callback.data == 'get_cat':
+        await callback.message.delete()
+        await bot.send_photo(callback.message.chat.id,
+                             photo=res['file'],
+                             reply_markup=GET_CAT)
+        # await bot.edit_message_media(res['file'],
+        #                              callback.message.chat.id,
+        #                              message_id=callback.message.message_id
+        #                              )
+    else:
+        try:
+            await bot.edit_message_text(text=f"Вот ссылка на вашу книгу:\n{str(call).lstrip('b')}".replace("'", ""),
+                                        chat_id=callback.message.chat.id,
+                                        message_id=callback.message.message_id)
+        except Exception as exc:
+            logger.info(exc)
+            print(f'error {exc}')
 
 
 def register_callback_cmd(dp: Dispatcher):
-    dp.register_callback_query_handler(callback_book_info)
+    dp.callback_query.register(callback_book_info)
+    dp.callback_query.register(callback_get_prev, Command(commands='get_prev'))
