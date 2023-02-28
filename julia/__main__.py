0886355a40dcb72c949b942acd6fbd2a05a0601c
Harlok <holmik90@yandex.ru>
2023-02-28 11:50:01 +0300

    ref(julia/__main__): ref to aiogram 3

--- julia/__main__.py
+++ julia/__main__.py
@@ -1,20 +1,33 @@
-from dispatcher import *
+import asyncio
+import logging
+import os
+from dotenv import load_dotenv, find_dotenv
+from aioredis import Redis
 
-# import pathlib
-# import logging
-# import asyncio
+from aiogram.fsm.storage.redis import RedisStorage
+from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 
-# from julia.dispatcher import *
-# from julia.handlers.message_handler import *
+from handlers import message_handler, cmd_handler, cmd_callback
 
 
-# async def start_bot(logger: logging.Logger = None) -> None:
-#     logging.basicConfig(level='INFO')
-#
-#     bot = Bot(token=os.getenv('TOKEN_API'))
-#     dp = Dispatcher(bot)
-#
-#     await dp.start_polling(bot)
+async def start_bot(logger: logging.Logger = None) -> None:
+    logging.basicConfig(level='INFO')
+    load_dotenv(find_dotenv())
+
+    redis = Redis()
+    storage = RedisStorage(redis=redis)
+
+    session = AiohttpSession()
+
+    bot = Bot(token=os.getenv('TOKEN_API'), session=session)
+    dp = Dispatcher(storage=storage)
+
+    cmd_callback.register_callback_cmd(dp)
+    cmd_handler.register_cmd_handler(dp)
+    message_handler.register_message_handler(dp)
+
+    await dp.start_polling(bot)
 
 
 # def setup_env() -> None:
@@ -26,17 +39,16 @@ from dispatcher import *
 #         load_dotenv(dotenv_path)
 
 
-# def main():
-#     logger = logging.getLogger(__name__)
-#     try:
-#         setup_env()
-#         asyncio.run(start_bot())
-#         logger.info('start')
-#     except (KeyboardInterrupt, SystemExit):
-#         pass
-#         logger.info('stop')
+def main():
+    logger = logging.getLogger(__name__)
+    try:
+        # setup_env()
+        asyncio.run(start_bot(logger))
+        logger.info('start')
+    except (KeyboardInterrupt, SystemExit):
+        pass
+        logger.info('stop')
 
 
 if __name__ == '__main__':
     main()
-
