73862966e048cab691ea1bb5e0e5e357987dd81b
Harlok <holmik90@yandex.ru>
2023-02-28 11:41:33 +0300

    feat(julia/keyboards): add BOOK_CHOICE_MENU

--- julia/keyboards/inline_keyboard.py
+++ julia/keyboards/inline_keyboard.py
@@ -1,8 +1,27 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+BOOK_CHOICE_MENU = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Болье - изучаем SQL', callback_data='beaulie')],
+        [InlineKeyboardButton(text='Фаулер - конкурентное программирование', callback_data='fauler')],
+        [InlineKeyboardButton(text='Brian Okken - Python Testing with pytest', callback_data='okken')]
+    ]
+)
+
 BOOK_MENU = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton('Бьюли - изучаем SQL', callback_data='Бьюли'),
-         InlineKeyboardButton('Фаулер - конкурентное программирование', callback_data='Фаулер')]
+        [InlineKeyboardButton(text='Назад', callback_data='prev_cmd')]
+    ]
+)
+
+GET_CAT = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Хочу еще котиков:3', callback_data='get_cat')]
+    ]
+)
+
+GET_GIF = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Больше гифок богу гифок', callback_data='get_gif')]
     ]
 )
