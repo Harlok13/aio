2211cd8e8918998398f58a1477e19b4f28efe2e8
Harlok <holmik90@yandex.ru>
2023-03-03 12:28:44 +0300

    feat(julia/handlers): add router without dispatcher
    rewrite to aiogram 3

--- julia/handlers/cmd_handler.py
+++ julia/handlers/cmd_handler.py
@@ -1,19 +1,22 @@
 import random
+import os
 
-from aiogram import types
+from aiogram import types, Router
 from aiogram.filters import Command
 import aiohttp
 
-from julia.dispatcher import *
 from julia.keyboards.reply_keyboard import *
 from julia.keyboards.inline_keyboard import *
 
 RANDOM_GIF = f'https://api.giphy.com/v1/gifs/random?api_key={os.getenv("GIPHY_API")}&tag=&rating=g'
 
+r = Router()
+
 
 async def start_cmd(message: types.Message):
     await message.answer(text='Выберите действие',
                          reply_markup=MENU_BOARD)
+    await message.delete()
 
 
 async def get_cat_cmd(message: types.Message):
@@ -38,8 +41,6 @@ async def get_gif_cmd(message: types.Message):
                 f'https://api.giphy.com/v1/gifs/search?api_key={os.getenv("GIPHY_API")}&q=cat&limit=25&offset={random.randrange(1000)}&rating=g&lang=en') as resp:
             res = await resp.json()
     await message.answer_animation(animation=res['data'][0]['images']['original']['mp4'])
-    # await bot.send_animation(message.chat.id,
-    #                          animation=res['data'][0]['images']['original']['mp4'])
     await message.delete()
 
 
@@ -49,9 +50,8 @@ async def info_cmd(message: types.Message):
 
 
 
-
-def register_cmd_handler(dp: Dispatcher):
-    dp.message.register(start_cmd, Command(commands=['start', 'help']))
-    dp.message.register(info_cmd, Command(commands=['info']))
-    dp.message.register(get_cat_cmd, Command(commands=['cat']))
-    dp.message.register(get_gif_cmd, Command(commands=['gif']))
+def register_cmd_handler(r: Router):
+    r.message.register(start_cmd, Command(commands=['start', 'help']))
+    # r.message.register(info_cmd, Command(commands=['info']))
+    r.message.register(get_cat_cmd, Command(commands=['cat']))
+    r.message.register(get_gif_cmd, Command(commands=['gif']))
