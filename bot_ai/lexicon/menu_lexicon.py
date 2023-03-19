aeeb444aac6bfb637b7b4d84f5014c744ae63362
harlok <holmik90@yandex.ru>
2023-03-19 22:48:15 +0400

    feat(./lexicon/menu_lexicon.py): add get_referral
    rename profile to get_profile

--- bot_ai/lexicon/menu_lexicon.py
+++ bot_ai/lexicon/menu_lexicon.py
@@ -1,5 +1,5 @@
-def profile(first_name, count_questions, tokens_count, refresh_time, balance, subscription) -> str:
-    msg: str = f'<b>📊Ваш профиль:</b>\n\n' \
+def get_profile(first_name, count_questions, tokens_count, refresh_time, balance, subscription) -> str:
+    profile_msg: str = f'<b>📊Ваш профиль:</b>\n\n' \
                f'<b>👤Имя:</b> {first_name}\n' \
                f'<b>🔸Задано вопросов:</b> {count_questions}\n' \
                f'<b>🔹Токенов осталось:</b> {tokens_count}\n' \
@@ -7,4 +7,12 @@ def profile(first_name, count_questions, tokens_count, refresh_time, balance, su
                f'<b>💰Баланс:</b> {balance}\n' \
                f'<b>💎Подписка:</b> {subscription}\n\n' \
                f'<em>❓Подробнее про токены! - /help</em>'
-    return msg
+    return profile_msg
+
+
+def get_referral(referral_count) -> str:
+    referral_msg: str = f'<b>📊Реферальная система:</b>\n\n' \
+                        f'<b>👤Рефералов:</b> {referral_count}\n\n' \
+                        f'<em>🔥За каждого приведенного пользователя вы получите 50₽:</em>\n\n' \
+                        f'<em>⚡Чтобы пригласить пользователя: нажмите кнопку <b>"ПРИГЛАСИТЬ"</b>:</em>'
+    return referral_msg
