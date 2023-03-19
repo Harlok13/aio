5016425355ef287df4dcfb3b08f26bcb9c685277
harlok <holmik90@yandex.ru>
2023-03-19 23:47:37 +0400

    feat(./lexicon/menu_lexicon.py): add get_search

--- bot_ai/lexicon/menu_lexicon.py
+++ bot_ai/lexicon/menu_lexicon.py
@@ -1,12 +1,15 @@
+from typing import Dict, Callable
+
+
 def get_profile(first_name, count_questions, tokens_count, refresh_time, balance, subscription) -> str:
     profile_msg: str = f'<b>📊Ваш профиль:</b>\n\n' \
-               f'<b>👤Имя:</b> {first_name}\n' \
-               f'<b>🔸Задано вопросов:</b> {count_questions}\n' \
-               f'<b>🔹Токенов осталось:</b> {tokens_count}\n' \
-               f'<b>⏳До восстановления:</b> {refresh_time}\n\n' \
-               f'<b>💰Баланс:</b> {balance}\n' \
-               f'<b>💎Подписка:</b> {subscription}\n\n' \
-               f'<em>❓Подробнее про токены! - /help</em>'
+                       f'<b>👤Имя:</b> {first_name}\n' \
+                       f'<b>🔸Задано вопросов:</b> {count_questions}\n' \
+                       f'<b>🔹Токенов осталось:</b> {tokens_count}\n' \
+                       f'<b>⏳До восстановления:</b> {refresh_time}\n\n' \
+                       f'<b>💰Баланс:</b> {balance}\n' \
+                       f'<b>💎Подписка:</b> {subscription}\n\n' \
+                       f'<em>❓Подробнее про токены! - /help</em>'
     return profile_msg
 
 
@@ -16,3 +19,11 @@ def get_referral(referral_count) -> str:
                         f'<em>🔥За каждого приведенного пользователя вы получите 50₽:</em>\n\n' \
                         f'<em>⚡Чтобы пригласить пользователя: нажмите кнопку <b>"ПРИГЛАСИТЬ"</b>:</em>'
     return referral_msg
+
+
+def get_search(current_search) -> str:
+    search_msg: str = f'<b>⭐РЕЖИМ ПОИСКА⭐</b>\n\n' \
+                      f'<b>🔶Активный режим поиска:</b>\n' \
+                      f'<b>{current_search}</b>\n\n'
+
+    return search_msg
