8099a5acc3c2777a410254b85d1952166ee84c9b
harlok <holmik90@yandex.ru>
2023-03-20 23:52:45 +0400

    feat(bot_ai/msg_handler.py): change to ChatCompletions

--- bot_ai/handlers/msg_handler.py
+++ bot_ai/handlers/msg_handler.py
@@ -3,21 +3,37 @@ import logging
 import openai
 
 from aiogram import Router
+from aiogram.filters import Text
 from aiogram.types import Message
 
 router = Router()
 
 
-@router.message()
+# @router.message(Text(contains="бот"))
+# async def ai_answer(message: Message) -> None:
+#     await message.reply('sec')
+#     logger = logging.getLogger(__name__)
+#     response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=message.text,
+#         temperature=1,
+#         max_tokens=3500,
+#     )
+#     logger.info(message.text)
+#     logger.info(message.from_user.first_name)
+#     logger.info(response.choices[0].text)
+#     print(response)
+#     await message.answer(response.choices[0].text)
+
+
+@router.message(Text(contains="бот"))
 async def ai_answer(message: Message) -> None:
-    logger = logging.getLogger(__name__)
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=message.text,
-        temperature=0.6,
-        max_tokens=1000,
+    await message.reply('sec')
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": f"{message.text}"}],
     )
+    logger = logging.getLogger(__name__)
     logger.info(message.text)
     logger.info(message.from_user.first_name)
-    logger.info(response.choices[0].text)
-    await message.answer(response.choices[0].text)
+    await message.answer(response.choices[0].message.content)
