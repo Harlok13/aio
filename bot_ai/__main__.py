e7236ce2b671eb13c8989150ae6dd69c34c1680c
harlok <holmik90@yandex.ru>
2023-03-22 17:04:11 +0400

    feat(./__main__.py): include postgresql

--- bot_ai/__main__.py
+++ bot_ai/__main__.py
@@ -1,14 +1,19 @@
 import asyncio
 import logging
+
 import openai
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.enums import ContentType
-from aiogram.filters import Command
+from sqlalchemy import URL
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.orm import sessionmaker
 
 from bot_ai import config
-from bot_ai.handlers import msg_handler, pay
-
+from bot_ai.data.engine import get_async_engine, get_session_maker
+from bot_ai.handlers import pay
+from bot_ai.handlers.cb_handler import register_cb_handlers
+from bot_ai.handlers.cmd_handler import register_cmd_handlers
+from bot_ai.handlers.pay import order, pre_checkout_query, successful_payment
 
 logger = logging.getLogger(__name__)
 
@@ -19,13 +24,31 @@ async def main() -> None:
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
 
-    bot: Bot = Bot(token=config.BOT_TOKEN)
+    bot: Bot = Bot(token=config.BOT_TOKEN, parse_mode='HTML')
     openai.api_key = config.AI_TOKEN
 
     dp: Dispatcher = Dispatcher()
 
     # register handlers
-    dp.include_router(msg_handler.router)
+    # dp.include_router(msg_handler.router)
+    register_cmd_handlers(dp)
+    register_cb_handlers(dp)
+
+    # register payment
+    dp.include_router(pay.router)
+    # dp.message.register(successful_payment, ContentTypesFilter(content_types=[ContentType.SUCCESSFUL_PAYMENT]))
+
+    # include postgresql
+    postgresql_url: URL = URL.create(
+        'postgresql+asyncpg',
+        username=config.PG_USER,
+        host=config.IP,
+        password=config.PG_PASSWORD,
+        database=config.DATABASE,
+        port=int(config.PG_PORT or 0)
+    )
+    async_engine: AsyncEngine = get_async_engine(postgresql_url)
+    async_session_maker: sessionmaker = get_session_maker(async_engine)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
