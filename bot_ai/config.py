1f9ea5a6895fb2b3a5f7f7963f215d16db0aa69e
harlok <holmik90@yandex.ru>
2023-03-22 18:38:34 +0400

    feat(./config.py): add postgres env var

--- bot_ai/config.py
+++ bot_ai/config.py
@@ -9,3 +9,9 @@ AI_TOKEN = os.getenv("AI_TOKEN")
 
 UKASSA_TOKEN = os.getenv("UKASSA_TOKEN")
 
+# postgresql
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+DATABASE = os.getenv("DATABASE")
+IP = os.getenv("IP")
+PG_PORT = os.getenv("PG_PORT")
