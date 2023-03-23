0207f7ed1e326f06ad2914a937eb2011d54c9c16
harlok <holmik90@yandex.ru>
2023-03-23 10:23:19 +0400

    feat(./user_requests.py): add set_days_left() and set_tokens()

--- bot_ai/utils/user_requsts.py
+++ bot_ai/utils/user_requsts.py
@@ -13,7 +13,58 @@ class UserRequest:
     def __init__(self, session: sessionmaker):
         self.session: sessionmaker = session
 
-    async def set_default_sub(self, status: str, user_id: int) -> None:
+    @staticmethod
+    def set_days_left(
+            current_status: str,
+            purchased_status: str,
+            days_left: int,
+            days_sub: int
+    ) -> int:
+        """
+        If the current status is the same as the one being purchased, then the number
+        of subscription days increases. Otherwise, the counter is reset to zero and takes
+        the standard value for the subscription
+        :param current_status: current status of the user subscription
+        :param purchased_status: purchased status of the user subscription
+        :param days_left: days left in the subscription
+        :param days_sub: number of days in the purchased subscription
+        :return: days left in the subscription
+        """
+        if current_status == purchased_status:
+            return days_left + days_sub
+        return days_sub
+
+    @staticmethod
+    def set_tokens(
+            current_status: str,
+            purchased_status: str,
+            current_tokens: int,
+            purchased_tokens
+    ) -> Union[str, int]:
+        """
+        If the current status is unlimited, then the number of tokens is unlimited.
+        if the current status matches the one being acquired, then the number of tokens
+        does not change. If the status is different, the number of tokens takes the
+        value that is in the subscription
+        :param current_status: current status of the user subscription
+        :param purchased_status: purchased status of the user subscription
+        :param current_tokens: current number of tokens of the user
+        :param purchased_tokens: purchased number of tokens of the user
+        :return: number of tokens or the string 'unlimited' if the status is unlimited
+        """
+        if purchased_status == 'unlimited':
+            return 'unlimited'
+        elif current_status == purchased_status and current_status:
+            return current_tokens
+        return purchased_tokens
+
+    async def set_subscription(
+            self,
+            purchased_status: str,
+            user_id: int,
+            days_sub: int,
+            purchased_tokens: Union[int, str],
+    ) -> None:
         """
         Set default subscription status in database
         :param status: subscription type to be set
