7fd815ed8b5c24db06621ce56171e21b7a1e7164
harlok <holmik90@yandex.ru>
2023-03-25 02:44:04 +0400

    feat(./config.py): add postgresql_test settings

--- bot_ai/config.py
+++ bot_ai/config.py
@@ -15,3 +15,10 @@ PG_PASSWORD = os.getenv("PG_PASSWORD")
 DATABASE = os.getenv("DATABASE")
 IP = os.getenv("IP")
 PG_PORT = os.getenv("PG_PORT")
+
+# postgresql_test
+PG_USER_TEST = os.getenv("PG_USER_TEST")
+PG_PASSWORD_TEST = os.getenv("PG_PASSWORD_TEST")
+DATABASE_TEST = os.getenv("DATABASE_TEST")
+IP_TEST = os.getenv("IP_TEST")
+PG_PORT_TEST = os.getenv("PG_PORT_TEST")
