e3463fd206a3b6e71cd9068e2ec2bfaf98d2e2a7
harlok <holmik90@yandex.ru>
2023-03-25 20:51:41 +0400

    ref(./bot_models.py): ref standard_bot_model()

--- bot_ai/utils/bot_models.py
+++ bot_ai/utils/bot_models.py
@@ -1,18 +1,42 @@
-from typing import Tuple
+from collections import namedtuple
+from typing import Tuple, Type
 
 import openai
 from aiogram.types import Message
 from openai.openai_object import OpenAIObject
 
+from bot_ai.handlers.cb_handler import standard_settings
 
-def standard_bot_model(message: Message) -> OpenAIObject:
+StandardSettings: Type['StandardSettings'] = namedtuple('StandardSettings', ['model', 'temperature', 'max_tokens'])
+
+# standard_settings: StandardSettings = StandardSettings(
+#     model='text-davinci-003',
+#     temperature=1,
+#     max_tokens=3500
+# )
+#
+#
+# def standard_bot_model(message: Message) -> OpenAIObject:
+#     response: OpenAIObject = openai.Completion.create(
+#         model=standard_settings.model,
+#         prompt=message.text,
+#         temperature=standard_settings.temperature,
+#         max_tokens=standard_settings.max_tokens,
+#     )
+#     return response
+
+
+
+
+def standard_bot_model(message: Message) -> Tuple[OpenAIObject, str]:
     response: OpenAIObject = openai.Completion.create(
-        model="text-davinci-003",
+        model=standard_settings.get('model'),
         prompt=message.text,
-        temperature=1,
-        max_tokens=3500,
+        temperature=standard_settings.get('temperature'),
+        max_tokens=standard_settings.get('max_tokens'),
     )
-    return response
+    msg: str = response.choices[0].text
+    return response, msg
 
 
 def artist_bot_model():
