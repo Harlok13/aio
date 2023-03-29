50dbeb54842feecd27c6100cfbf9aadff1380ebb
harlok <holmik90@yandex.ru>
2023-03-29 14:52:02 +0400

    feat(./cb_handlers_pay.py): add test openai settings

--- bot_ai/handlers/cb_handler.py
+++ bot_ai/handlers/cb_handler.py
@@ -1,13 +1,24 @@
+import asyncio
 import logging
-from typing import Dict, Callable, Awaitable, Optional
+from typing import Dict, Callable, Awaitable, Optional, List
+from collections import deque
 
-from aiogram import Router, F
-from aiogram.types import CallbackQuery
+from aiogram import Router, F, Bot
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery, Message, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-from bot_ai.keyboards.inline_keyboard import main_menu
+from bot_ai.keyboards.inline_keyboard import main_menu, change_model_menu
+from bot_ai.lexicon.menu_lexicon import MENU_LEXICON, get_models_info
+from bot_ai.states.states import FSMOpenaiModel
+from bot_ai.utils.current_state_info import get_current_state_info
 from bot_ai.utils.user_requsts import UserRequest
-from feature_test.menu_lexicon_test import MENU_LEXICON
-from bot_ai.utils.user_requsts import UserRequest
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+# initialize deque for set_openai_model_state_cb
+_set_model_deque: deque = deque()
+
 
 logger = logging.getLogger(__name__)
 
@@ -31,5 +42,97 @@ async def main_menu_cb(callback: CallbackQuery, request: UserRequest) -> Optiona
     )
 
 
+####################### test #######################
+async def set_openai_model_state_cb(callback: CallbackQuery, state: FSMContext, bot: Bot) -> None:
+    """
+
+    """
+    global _set_model_deque
+    logger.info(callback.data)
+    m = {
+        'set_standard': FSMOpenaiModel.set_standard,
+        'set_companion': FSMOpenaiModel.set_companion,
+        'set_artist': FSMOpenaiModel.set_artist,
+        'set_coder': FSMOpenaiModel.set_coder,
+        'set_translator': FSMOpenaiModel.set_translator,
+    }
+    await state.set_state(m.get(callback.data))
+
+    current_state: str = await state.get_state()
+
+    await callback.message.edit_text(
+        get_models_info(current_state.split(':')[-1]),
+        reply_markup=main_menu('cat_models')
+    )
+
+    model_choice: Message = await callback.message.answer(
+        text='Выбрана модель: ' + current_state,
+    )
+    _set_model_deque.append(model_choice.message_id)
+
+    await asyncio.sleep(3)
+    await bot.delete_message(callback.message.chat.id, _set_model_deque.popleft())
+
+
+standard_settings = {
+    'model': 'text-davinci-003',
+    'temperature': 1.0,
+    'max_tokens': 3500
+}
+
+
+async def _change_temperature(callback: CallbackQuery):
+    global standard_settings
+    param = standard_settings['temperature']
+    if callback.data == 'get_temperature_plus' and param < 1.0:
+        standard_settings['temperature'] = (param * 10 + 1) / 10
+    elif callback.data == 'get_temperature_minus' and param > 0:
+        standard_settings['temperature'] = (param * 10 - 1) / 10
+    else:
+        await callback.answer('Нельзя изменить')
+
+
+async def _change_tokens(callback: CallbackQuery):
+    global standard_settings
+    param = standard_settings['max_tokens']
+    if callback.data == 'get_tokens_minus' and param > 0:
+        standard_settings['max_tokens'] = param - 500
+    elif callback.data == 'get_tokens_plus' and param < 4000:
+        standard_settings['max_tokens'] = param + 500
+    else:
+        await callback.answer('Нельзя изменить')
+
+
+async def change_openai_model_cb(callback: CallbackQuery, state: FSMContext) -> None:
+    global standard_settings
+    current_state: str = await state.get_state()
+    await _change_temperature(callback)
+    await _change_tokens(callback)
+
+    current_temperature: float = standard_settings['temperature']
+    current_tokens: int = standard_settings['max_tokens']
+    await callback.message.edit_text(
+        text=MENU_LEXICON.get('change_models')(current_state, current_temperature, current_tokens),
+        reply_markup=change_model_menu(callback.data)
+    )
+
+
+async def set_parameters_cb(callback: CallbackQuery, state: FSMContext) -> None:
+    global standard_settings
+    current_state: str = await state.get_state()
+    await _change_temperature(callback)
+    await _change_tokens(callback)
+
+    current_temperature: float = standard_settings['temperature']
+    current_tokens: int = standard_settings['max_tokens']
+    await callback.message.edit_text(
+        text=MENU_LEXICON.get('change_models')(current_state, current_temperature, current_tokens),
+        reply_markup=change_model_menu('change_standard')
+    )
+######################## end test #######################
+
 def register_cb_handlers(router: Router) -> None:
-    router.callback_query.register(main_menu_cb, F.data.startswith('cat'))
+    router.callback_query.register(get_main_menu_cb, F.data.startswith('cat'))
+    router.callback_query.register(set_openai_model_state_cb, F.data.startswith('set'))
+    router.callback_query.register(change_openai_model_cb, F.data.startswith('change'))
+    router.callback_query.register(set_parameters_cb, F.data.startswith('get'))
