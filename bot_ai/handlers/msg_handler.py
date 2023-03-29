44ae10694b14a5ab085b8bd4c939c6cba1e28dfc
harlok <holmik90@yandex.ru>
2023-03-29 14:39:09 +0400

    feat(msg_handler.py): add default_model_answer()

--- bot_ai/handlers/msg_handler.py
+++ bot_ai/handlers/msg_handler.py
@@ -1,34 +1,34 @@
 import logging
-from typing import Tuple, Callable
 
-import openai
-
-from aiogram import Router
-from aiogram.filters import Text
+from aiogram import Router, F
+from aiogram.filters import StateFilter
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
-from openai.openai_object import OpenAIObject
 
-from bot_ai.utils.bot_models import companion_bot_model
+from bot_ai.states.states import FSMOpenaiModel
+from bot_ai.utils.bot_models import OPENAI_MODELS
+from bot_ai.utils.current_state_info import get_current_state_info
 from bot_ai.utils.user_requsts import UserRequest
 
 router = Router()
 logger = logging.getLogger(__name__)
 
-# @router.message(Text(contains="бот"))
-# async def ai_answer(message: Message) -> None:
-#     await message.reply('sec')
-#     logger = logging.getLogger(__name__)
-#     response = openai.Completion.create(
-#         model="text-davinci-003",
-#         prompt=message.text,
-#         temperature=1,
-#         max_tokens=3500,
-#     )
-#     logger.info(message.text)
-#     logger.info(message.from_user.first_name)
-#     logger.info(response.choices[0].text)
-#     print(response)
-#     await message.answer(response.choices[0].text)
+
+@router.message(StateFilter(FSMOpenaiModel.set_standard))
+async def default_model_answer(message: Message, request: UserRequest, state: FSMContext) -> None:
+    print(current_state := await state.get_state(), 'default state')
+    await message.reply('sec')
+    if check_user_tokens := await request.check_user_tokens(message.from_user.id):
+        response, msg = OPENAI_MODELS.get(get_current_state_info(current_state))(message)
+        await request.increase_question_count(user_id=message.from_user.id)
+
+        logger.info(message.text)
+        logger.info(message.from_user.first_name)
+        logger.info(response.choices[0].text)
+
+        await message.answer(msg)
+    else:
+        await message.answer('У вас недостаточно токенов')
 
 
 @router.message()
