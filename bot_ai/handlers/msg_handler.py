5b70cd6d165072c1ff84810a4be2fb9ef879784d
harlok <holmik90@yandex.ru>
2023-03-29 14:41:05 +0400

    fix(msg_handler.py): fix companion_model_answer()

--- bot_ai/handlers/msg_handler.py
+++ bot_ai/handlers/msg_handler.py
@@ -14,6 +14,7 @@ router = Router()
 logger = logging.getLogger(__name__)
 
 
+# ref
 @router.message(StateFilter(FSMOpenaiModel.set_standard))
 async def default_model_answer(message: Message, request: UserRequest, state: FSMContext) -> None:
     print(current_state := await state.get_state(), 'default state')
@@ -31,21 +32,24 @@ async def default_model_answer(message: Message, request: UserRequest, state: FS
         await message.answer('У вас недостаточно токенов')
 
 
-@router.message()
-async def companion_bot_answer(message: Message, request: UserRequest, model: Callable) -> None:
+# ref
+@router.message(StateFilter(FSMOpenaiModel.set_companion))
+async def companion_model_answer(message: Message, request: UserRequest, state: FSMContext) -> None:
+    print(current_state := await state.get_state(), 'state companion')
+
     await message.reply('sec')
-    check_user_tokes = await request.check_user_tokens(message.from_user.id)
-    if check_user_tokes:
-        response, msg = model(message)
+
+    if check_user_tokens := await request.check_user_tokens(message.from_user.id):
+        response, msg = OPENAI_MODELS.get(get_current_state_info(current_state))(message)
         await request.increase_question_count(user_id=message.from_user.id)
 
         logger.info(message.text)
         logger.info(message.from_user.first_name)
-        print(response)
 
         total_openai_spent_tokens: int = response.usage.total_tokens
         await request.decrease_user_tokens(
-            user_id=message.from_user.id, openai_spent_tokens=total_openai_spent_tokens
+            user_id=message.from_user.id,
+            openai_spent_tokens=total_openai_spent_tokens
         )
         await message.answer(msg)
     else:
